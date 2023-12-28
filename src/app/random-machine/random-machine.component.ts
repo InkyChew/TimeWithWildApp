2bb725b8905823ca58b48359ac20e192e6660ceb
InkyChew <inky11308@gmail.com>
2023-12-28 14:39:17 +0800

    control stop

--- src/app/random-machine/random-machine.component.ts
+++ src/app/random-machine/random-machine.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable, concatMap, delay, observable, of, switchMap, timer } from 'rxjs';
 const HEIGHT: number = 300;
 
 @Component({
@@ -11,7 +12,7 @@ export class RandomMachineComponent implements OnInit {
 
   ideaList?: Idea[];
   targetIdea?: Idea;
-  transform: string = "translateY(0px)";
+  isStart: boolean = false;
 
   constructor(private _http: HttpClient) { }
 
@@ -21,30 +22,70 @@ export class RandomMachineComponent implements OnInit {
       this.targetIdea = res[0];
     });
   }
-
-  reset() {
-    if(this.ideaList) {
-      this.transform = `translateY(0px)`;
-      this.targetIdea = this.ideaList[0];
+  
+  transform(state: SlideState): string {
+    let stt = "";
+    switch(state) {
+      case SlideState.Prev:
+        stt = "prev";
+        break;
+      case SlideState.Active:
+        stt = "active";
+        break;
+      case SlideState.Next:
+        stt = "next";
+        break;
     }
+    return stt;
   }
 
   start() {
     if(this.ideaList) {
-      let i = 0;
-      const r = Math.floor(Math.random() * this.ideaList.length);
-      const y = HEIGHT * (this.ideaList.length-1);
-      while(i <= r) {
-        this.ideaList?.push(this.ideaList[0]);
-        this.ideaList?.splice(0, 1);
-        i++;
+      this.isStart = true;
+      this.ideaList[0].state = SlideState.Prev;
+      this.ideaList[1].state = SlideState.Active;
+    }
+  }
+  
+  stop() {
+    this.isStart = false;
+  }
+
+  tend(idea: Idea) {
+    if(this.ideaList) {
+      switch(idea.state) {
+        case SlideState.Prev:
+          idea.state = SlideState.None;
+          break;
+        case SlideState.Active:
+          idea.state = SlideState.None;
+          of(null).pipe(
+            switchMap(() => {
+              this.ideaList?.push(this.ideaList[0]);
+              this.ideaList?.splice(0, 1);
+              return of(null).pipe(delay(0));
+            })
+          ).subscribe(() => {
+            if(this.isStart) this.start();
+            else this.targetIdea = idea;
+          });
+        break;
       }
-      this.transform = `translateY(${-y}px)`;
     }
   }
 }
 
-export interface Idea {
+export interface Slide {
+  state: SlideState
+}
+
+export enum SlideState {
+  None,
+  Prev,
+  Active,
+  Next
+}
+export interface Idea extends Slide {
   id: number,
   type: string,
   name: string,
